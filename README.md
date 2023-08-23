9204015f6c9b5b08f200ae28801f3b992dcb23bd
Luis Castano <69082910+luis-castano@users.noreply.github.com>
2023-08-23 10:40:38 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# shopping-list
+
+Shopping List project from YouTube: https://www.youtube.com/watch?v=UFD4SP91tSM&list=PLcSrlX4GIkQDUzxMMgpHSBFD6bSha_PJY
---
+++ app.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://shopping-list-40d2c-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+
+  push(shoppingListInDB, inputValue);
+  clearInputFieldEl();
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let cuttentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+
+      appendItemToShoppingListEl(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet";
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = "";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(item) {
+  // shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+
+  let itemID = item[0];
+  let itemValue = item[1];
+  let newEl = document.createElement("li");
+
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let exactLocationfItemInDB = ref(database, `shoppingList/${itemID}`);
+
+    remove(exactLocationfItemInDB);
+  });
+
+  shoppingListEl.append(newEl);
+}
+
+// https://www.youtube.com/watch?v=UFD4SP91tSM&list=PLcSrlX4GIkQDUzxMMgpHSBFD6bSha_PJY&index=1 1 10 26
